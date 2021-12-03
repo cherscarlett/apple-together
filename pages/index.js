487c2374cd91d18506bd7b9231a2c76c45ef4d6e
Cher Scarlett <me@cher.dev>
2021-12-03 07:19:36 -0800

    footer

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
             </ul>
           </nav>
           <small className={styles.small}>
-            <a href="//appletoo.us/stories">#AppleToo Stories</a>
+            <a href="//appletoo.us/stories">#AppleToo Stories</a>•<a href="//appletoo.us/letter">#AppleToo Open Letter</a>
           </small>
         </div>
       </main>
