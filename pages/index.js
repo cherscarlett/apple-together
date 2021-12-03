24875e8fc31e328832a5fdb30b57de505eb833f8
Cher Scarlett <me@cher.dev>
2021-12-03 09:54:51 -0800

    content

--- pages/index.js
+++ pages/index.js
@@ -24,13 +24,10 @@ export default function Home() {
             At Apple, we make incredible products and services. We pour ourselves into that work, whether we are enriching customers&apos; lives at the Apple Store, animating rain for the next Keynote, plating lunch at Caffè Macs, or chatting with a customer to transfer their family videos from their iPhone to their MacBook.
           </p>
           <p>
-            Apple prides itself on its commitment to diversity, equity, and an environment where everyone can do their best work. But Apple has fallen short of this goal for so many of our current and former teammates.
+            Apple prides itself on its commitment to diversity, equity, and an environment where everyone can do their best work. But Apple has fallen short of this goal for so many of our current and former teammates, so we gave them space to share those stories with #AppleToo.
           </p>
           <p>
-            We gathered our hurt teammates together to give space to tell their stories with #AppleToo, and have written letters together to make requests of our leadership.
-          </p>
-          <p>
-            In Slack, we're seeing an increase of rules restricting our ability to continue collaborating on workplace issues that are important to us. We're making our own space off of our virtual campus to ensure our voices can still to be heard.
+            In Slack, we have written letters together to make requests of our leadership. However, we're seeing an increase of rules restricting our ability to continue collaborating on workplace issues that are important to us. We're making our own space off of our virtual campus to ensure our voices can still to be heard.
           </p>
           <p>
             Apple celebrates its footprint of over 2.4 million workers. That&apos;s us: including more than 100,000 direct employees in the United States working under 22 executives.
