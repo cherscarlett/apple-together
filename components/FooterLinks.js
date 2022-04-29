0a570c6147412cd3689f095a4a267f439d0d0fa6
Joachim Kurz <joachim@kurz.online>
2022-04-29 06:22:12 -0700

    typos + a bit f highlighting

--- components/FooterLinks.js
+++ components/FooterLinks.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function FooterLinks() {
     return (
         <footer className={styles.small}>
-            <Link href="/hotnews/thoughts-on-office-bound-work">Thoughts on Office-Bound Work</Link>
+            <Link href="/hotnews/thoughts-on-office-bound-work">Thoughts on Office-bound Work</Link>
             
             <Link href="/shareholders">Shareholder Vote Guide</Link>
             <a href="//appletoo.us/stories">#AppleToo Stories</a>
